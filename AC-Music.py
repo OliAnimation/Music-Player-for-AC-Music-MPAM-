e7d8dff3ff6e7562bbe695f1e6ecb812670c4267
OliAnimation <57059457+OliAnimation@users.noreply.github.com>
2020-04-17 18:02:47 +1000

    Add files via upload

---
+++ AC-Music.py
@@ -0,0 +1,81 @@
+import datetime
+from playsound import playsound
+currentDT = datetime.datetime.now()
+time_hr = currentDT.strftime("%I %p")
+directory = input('Folder Directory ')
+while True:
+        currentDT = datetime.datetime.now()
+        time_hr = currentDT.strftime("%I %p")
+        mp3_to_play = directory + '/' + time_hr + '.mp3'
+        if time_hr == '12 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '01 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '02 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '03 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '04 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '05 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '06 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '07 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '08 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '09 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '10 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '11 AM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '12 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '01 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '02 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '03 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '04 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '05 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '06 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '07 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '08 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '09 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '10 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")
+        elif time_hr == '11 PM' :
+                playsound(mp3_to_play)
+                time_hr = currentDT.strftime("%I %p")

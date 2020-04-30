4010924a6757c97ed73e0857467870a0231fd0fe
OliAnimation <57059457+OliAnimation@users.noreply.github.com>
2020-04-30 16:32:23 +1000

    1.0 Update!

--- AC-Music.py
+++ AC-Music.py
@@ -1,10 +1,18 @@
 import os
 import datetime
-from playsound import playsound
+import time
+from pygame import mixer
+from mutagen.mp3 import MP3
 
+os.add_dll_directory('C:\Program Files\VideoLAN\VLC')
 directory = input('Folder Directory ')
 while True:
         currentDT = datetime.datetime.now()
         time_hr = currentDT.strftime("%I %p")
         mp3_to_play = os.path.join(directory, time_hr + '.mp3')
-        playsound(mp3_to_play)
+        mixer.init()
+        mixer.music.load(mp3_to_play)
+        song = MP3(mp3_to_play)
+        mixer.music.play()
+        if mixer.music.get_busy() == 1:
+                time.sleep(song.info.length)
